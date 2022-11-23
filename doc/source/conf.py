c632da5c3699220ac5acba1c51f022b5fa431426
Camille <camille.latapie@ansys.com>
2022-11-23 14:53:22 +0100

    Deleting an empty folder and modifying html title

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@ release = version = "0.1.dev0"
 # Select desired logo, theme, and declare the html title
 html_logo = logo
 html_theme = "ansys_sphinx_theme"
-html_short_title = html_title = "pymapdl-techdemos"
+html_short_title = html_title = "PyMAPDL Techdemos"
 
 # specify the location of your github repo
 html_theme_options = {
