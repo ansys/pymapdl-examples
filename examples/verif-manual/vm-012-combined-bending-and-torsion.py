14bd4396973b872fa1dfa3b9c22baae5fee78a3f
Camille <78221213+clatapie@users.noreply.github.com>
2023-07-05 18:51:23 +0200

    doc: improving `vm12.py`

--- examples/verif-manual/vm-012-combined-bending-and-torsion.py
+++ examples/verif-manual/vm-012-combined-bending-and-torsion.py
@@ -50,6 +50,7 @@ Analysis assumptions and modeling notes:
 # sphinx_gallery_thumbnail_path = '_static/vm12_setup.png'
 
 from ansys.mapdl.core import launch_mapdl
+import pandas
 
 ###############################################################################
 # Start MAPDL
@@ -59,8 +60,9 @@ mapdl = launch_mapdl(loglevel="WARNING", print_com=True)
 mapdl.clear()  # optional as MAPDL just started
 
 ###############################################################################
-# Enter verification example mode and the pre-processing routine
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Pre-processing with ET PIPE16
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 mapdl.verify("vm12")
 mapdl.prep7()
 
@@ -76,6 +78,11 @@ mapdl.d(1, "ALL")
 mapdl.f(2, "MZ", 9000)
 mapdl.f(2, "FX", -250)
 mapdl.finish()
+
+###############################################################################
+# Solve
+# ~~~~~
+
 mapdl.slashsolu()
 mapdl.outpr("BASIC", 1)
 with mapdl.non_interactive:
@@ -88,18 +95,36 @@ with mapdl.non_interactive:
     mapdl.run("/OUT")
     mapdl.run("/GOPR")
 mapdl.last_response
-mapdl.get("P_STRESS", "ELEM", 1, "ETAB", "P_STRS")
-mapdl.get("SHEAR", "ELEM", 1, "ETAB", "SHR")
-mapdl.dim("LABEL", "CHAR", 2, 2)
-mapdl.dim("VALUE", "", 2, 3)
-mapdl.run("LABEL(1,1) = 'MAX PRIN','MAX SH S'")
-mapdl.run("LABEL(1,2) = 'STRS psi','TRS psi '")
-mapdl.vfill("VALUE(1", "1)", "DATA", 7527, 3777)
-mapdl.vfill("VALUE(1", "2)", "DATA", "P_STRESS", "(SHEAR/2)")
-mapdl.vfill("VALUE(1", "3)", "DATA", "ABS(P_STRESS/7527 )", "ABS((SHEAR/2)/3777 )")
-mapdl.save("TABLE_1")
-mapdl.run("FINI")
-mapdl.run("/CLEAR,NOSTART")
+
+###############################################################################
+# Post-processing
+# ~~~~~~~~~~~~~~~
+
+p_stress = mapdl.get("P_STRESS", "ELEM", 1, "ETAB", "P_STRS")
+shear = mapdl.get("SHEAR", "ELEM", 1, "ETAB", "SHR")
+p_trs = shear / 2
+
+# Fill the array with target values
+target_p_stress = 7527
+target_p_trs = 3777
+
+data = [
+    [target_p_stress, p_stress, abs(p_stress / target_p_stress)],
+    [target_p_trs, p_trs, abs(p_trs / target_p_trs)],
+]
+col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
+row_headers = ["MAX PRINSTRS psi", "MAX SH STRS psi"]
+
+###############################################################################
+# Verify the results.
+# ~~~~~~~~~~~~~~~~~~~
+
+print(pandas.DataFrame(data, row_headers, col_headers))
+
+
+###############################################################################
+# Pre-processing with ET PIPE288
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 mapdl.prep7()
 mapdl.run("C***     USING PIPE288")
@@ -118,6 +143,11 @@ mapdl.f(2, "MZ", 9000)
 mapdl.f(2, "FX", -250)
 mapdl.finish()
 
+
+###############################################################################
+# Solve
+# ~~~~~
+
 mapdl.slashsolu()
 mapdl.outpr("BASIC", 1)
 with mapdl.non_interactive:
@@ -135,41 +165,32 @@ with mapdl.non_interactive:
 mapdl.get("P_STRESS", "PLNSOL", 0, "MAX")
 mapdl.plesol("S", "INT")
 mapdl.get("SHEAR", "PLNSOL", 0, "MAX")
-mapdl.dim("LABEL", "CHAR", 2, 2)
-mapdl.dim("VALUE", "", 2, 3)
-mapdl.run("LABEL(1,1) = 'MAX PRIN','MAX SH S'")
-mapdl.run("LABEL(1,2) = 'STRS psi','TRS psi '")
-mapdl.vfill("VALUE(1", "1)", "DATA", 7527, 3777)
-mapdl.vfill("VALUE(1", "2)", "DATA", "P_STRESS", "(SHEAR/2)")
-mapdl.vfill("VALUE(1", "3)", "DATA", "ABS(P_STRESS/7527 )", "ABS((SHEAR/2)/3777 )")
-mapdl.save("TABLE_2")
-mapdl.run("FINI")
-mapdl.run("/CLEAR,NOSTART")
-mapdl._run("/NOPR")  # It is not recommended to use '/NOPR' in a normal PyMAPDL session.
-mapdl.com("")
 
-with mapdl.non_interactive:
-    mapdl.run("/OUT,vm12,vrt")
-    mapdl.com("------------------- VM12 RESULTS COMPARISON ---------------------")
-    mapdl.com("")
-    mapdl.com("|   TARGET   |   Mechanical APDL   |   RATIO")
-    mapdl.com("")
-    mapdl.resume("TABLE_1")
-    mapdl.com("USING PIPE16")
-    mapdl.com("")
-    mapdl.run("*VWRITE,LABEL(1,1),LABEL(1,2),VALUE(1,1),VALUE(1,2),VALUE(1,3)")
-    mapdl.run("(1X,A8,A8,'   ',F10.0,'  ',F12.0,'   ',1F15.3)")
-    mapdl.com("")
-    mapdl.resume("TABLE_2")
-    mapdl.com("USING PIPE288")
-    mapdl.com("")
-    mapdl.run("*VWRITE,LABEL(1,1),LABEL(1,2),VALUE(1,1),VALUE(1,2),VALUE(1,3)")
-    mapdl.run("(1X,A8,A8,'   ',F10.0,'  ',F12.0,'   ',1F15.3)")
-    mapdl.com("")
-    mapdl.com("-----------------------------------------------------------------")
-    mapdl.run("/OUT")
+###############################################################################
+# Post-processing
+# ~~~~~~~~~~~~~~~
 
-mapdl.run("/GOPR")
+p_stress = mapdl.get("P_STRESS", "PLNSOL", 0, "MAX")
+mapdl.plesol("S", "INT")
+shear = mapdl.get("SHEAR", "PLNSOL", 0, "MAX")
+p_trs = shear / 2
+
+# Fill the array with target values
+target_p_stress = 7527
+target_p_trs = 3777
+
+data = [
+    [target_p_stress, p_stress, abs(p_stress / target_p_stress)],
+    [target_p_trs, p_trs, abs(p_trs / target_p_trs)],
+]
+col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
+row_headers = ["MAX PRINSTRS psi", "MAX SH STRS psi"]
+
+###############################################################################
+# Verify the results.
+# ~~~~~~~~~~~~~~~~~~~
+
+print(pandas.DataFrame(data, row_headers, col_headers))
 
 mapdl.finish()
 mapdl.starlist("vm12", "vrt")
