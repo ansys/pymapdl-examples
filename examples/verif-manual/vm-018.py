Minor fix for result calculation table

examples/verif-manual/vm-018.py
@@ -53,6 +53,7 @@ Analysis Assumptions and Modeling Notes:
 
 # Importing the `launch_mapdl` function from the `ansys.mapdl.core` module
 from ansys.mapdl.core import launch_mapdl
+import numpy as np
 import pandas as pd
 
 # Launch MAPDL with specified settings
@@ -171,13 +172,10 @@ target_val = [-2.648, 6366, -3183]
 # Fill result values
 sim_res = [def_z, strss_b, strss_t]
 
-data = [
-    [target_val[0], sim_res[0], abs(sim_res[0] / target_val[0])],
-    [target_val[1], sim_res[1], abs(sim_res[1] / target_val[1])],
-    [target_val[2], sim_res[2], abs(sim_res[2] / target_val[2])],
-]
 col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
 row_headers = ["Deflection (in)", "Stress_Bend (psi)", "Shear Stress (psi)"]
+
+data = [target_val, sim_res, np.abs(target_val) / np.abs(sim_res)]
 
 results = f"""
 
@@ -191,7 +189,7 @@ PIPE18:
 # ~~~~~~~~~~~~~~~~~~~
 
 print(results)
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
 ###############################################################################
 # Finish the post-processing processor.
@@ -301,10 +299,13 @@ strs_ben = mapdl.etable("STRS_BEN", "SMISC", 35)
 # Get bending stresses (ETAB: STRS_BEN) for element 1 using ETABLE command
 strss_b = mapdl.get("STRSS_B", "ELEM", 1, "ETAB", "STRS_BEN")
 
+# for graphics displays
+mapdl.show()
 # Plot elemtal solution values for SXY component
 mapdl.plesol("S", "XY")
 # Get minimum shear stress
 shear_sxy = mapdl.get("SHEAR", "PLNSOL", 0, "MIN")
+mapdl.show("close")
 
 # Set target values
 target_val = [-2.648, 6366, -3183]
@@ -312,13 +313,10 @@ target_val = [-2.648, 6366, -3183]
 # Fill result values
 sim_res = [def_z, strss_b, shear_sxy]
 
-data = [
-    [target_val[0], sim_res[0], abs(sim_res[0] / target_val[0])],
-    [target_val[1], sim_res[1], abs(sim_res[1] / target_val[1])],
-    [target_val[2], sim_res[2], abs(sim_res[2] / target_val[2])],
-]
 col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
 row_headers = ["Deflection (in)", "Stress_Bend (psi)", "Shear Stress (psi)"]
+
+data = [target_val, sim_res, np.abs(target_val) / np.abs(sim_res)]
 
 results = f"""
 
@@ -330,7 +328,7 @@ PIPE289:
 # ~~~~~~~~~~~~~~~~~~~
 
 print(results)
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
 ###############################################################################
 # Finish the post-processing processor.

examples/verif-manual/vm-020.py
@@ -46,6 +46,7 @@ Analysis Assumptions and Modeling Notes:
 
 # Importing the `launch_mapdl` function from the `ansys.mapdl.core` module
 from ansys.mapdl.core import launch_mapdl
+import numpy as np
 import pandas as pd
 
 # Launch MAPDL with specified options
@@ -171,12 +172,10 @@ target_stress = [15000, 29749]
 # Fill result values
 sim_res = [strs_hop, strs_ax]
 
-data = [
-    [target_stress[0], sim_res[0], abs(sim_res[0]) / target_stress[0]],
-    [target_stress[1], sim_res[1], abs(sim_res[1]) / target_stress[1]],
-]
 col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
 row_headers = ["Stress_1 (psi)", "Stress_2 (psi)"]
+
+data = [target_stress, sim_res, np.abs(target_stress) / np.abs(sim_res)]
 
 results = f"""
 
@@ -189,7 +188,7 @@ results = f"""
 # ~~~~~~~~~~~~~~~~~~~
 
 print(results)
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
 ###############################################################################
 # Finish the post-processing processor.

examples/verif-manual/vm-025.py
@@ -51,6 +51,7 @@ Analysis Assumptions and Modeling Notes:
 
 # Importing the `launch_mapdl` function from the `ansys.mapdl.core` module
 from ansys.mapdl.core import launch_mapdl
+import numpy as np
 import pandas as pd
 
 # Launching MAPDL with specified settings
@@ -257,41 +258,28 @@ data = [
 print(results)
 print(pd.DataFrame(data, row_headers, col_headers))
 
+# Radial stress results comparison
 row_headers = [
     "Stress_r, psi (r = 4 in)",
     "Stress_r, psi (r = 6 in)",
     "Stress_r, psi (r = 8 in)",
 ]
-data = [
-    [target_strss[0], res_strss[0], abs(target_strss[0] / res_strss[0])],
-    [target_strss[1], res_strss[1], abs(target_strss[1] / res_strss[1])],
-    [target_strss[2], res_strss[2], abs(target_strss[2] / res_strss[2])],
-]
-print(pd.DataFrame(data, row_headers, col_headers))
 
+data = [target_strss, res_strss, np.abs(target_strss) / np.abs(res_strss)]
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
+
+# Tangential stress results comparison
 row_headers = [
     "Stress_t, psi (r = 4 in)",
     "Stress_t, psi (r = 6 in)",
     "Stress_t, psi (r = 8 in)",
 ]
 data = [
-    [
-        target_tst_strss[0],
-        res_tst_strss[0],
-        abs(target_tst_strss[0] / res_tst_strss[0]),
-    ],
-    [
-        target_tst_strss[1],
-        res_tst_strss[1],
-        abs(target_tst_strss[1] / res_tst_strss[1]),
-    ],
-    [
-        target_tst_strss[2],
-        res_tst_strss[2],
-        abs(target_tst_strss[2] / res_tst_strss[2]),
-    ],
+    target_tst_strss,
+    res_tst_strss,
+    np.abs(target_tst_strss) / np.abs(res_tst_strss),
 ]
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
 
 ###############################################################################
@@ -382,29 +370,24 @@ RESULTS FOR CASE Rotation = 1000 rad/sec:
 """
 
 col_headers = ["TARGET", "Mechanical APDL", "RATIO"]
+
+# Radial stress results comparison
 row_headers = ["Stress_r, psi (r = 4 in)", "Stress_r, psi (r = 5.43 in)"]
-data = [
-    [target_strss[0], res_strss[0], abs(target_strss[0] / res_strss[0])],
-    [target_strss[1], res_strss[1], abs(target_strss[1] / res_strss[1])],
-]
+
+data = [target_strss, res_strss, np.abs(target_strss) / np.abs(res_strss)]
 
 print(results)
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
+# Tangential stress results comparison
 row_headers = ["Stress_t, psi (r = 4 in)", "Stress_t, psi (r = 5.43 in)"]
+
 data = [
-    [
-        target_tst_strss[0],
-        res_tst_strss[0],
-        abs(target_tst_strss[0] / res_tst_strss[0]),
-    ],
-    [
-        target_tst_strss[1],
-        res_tst_strss[1],
-        abs(target_tst_strss[1] / res_tst_strss[1]),
-    ],
+    target_tst_strss,
+    res_tst_strss,
+    np.abs(target_tst_strss) / np.abs(res_tst_strss),
 ]
-print(pd.DataFrame(data, row_headers, col_headers))
+print(pd.DataFrame(np.transpose(data), row_headers, col_headers))
 
 ###############################################################################
 # Finish the post-processing processor.