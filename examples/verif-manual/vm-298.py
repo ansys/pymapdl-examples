365eaa831746a40e825eadacb7b65b8c56cc158a
clatapie <78221213+clatapie@users.noreply.github.com>
2026-01-27 19:10:22 +0100

    fix: example

--- examples/verif-manual/vm-298.py
+++ examples/verif-manual/vm-298.py
@@ -289,7 +289,7 @@ for j in range(1, _N):
         COEFV = (4 * _vj * _vk) * (2 * _KO * _Ur**2)
         COEFU = _uj * _uk
         for i in range(FREQ_PTS):
-            OMG = OMG_ARRAY[i]
+            OMG = float(OMG_ARRAY[i])
             TERM1 = (600 * OMG / (_PI * _Ur)) ** 2
             TERM1 = TERM1 / (1 + TERM1) ** (4 / 3)
             EXPO = -(_C1 * OMG * (abs(j - k)) * _H) / (2 * _PI * _Ur)
